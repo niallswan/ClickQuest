a5ce960b9ac972e7070806934db7ff64344e5a44
Niall Swan <niallswan@outlook.com>
2024-01-02 22:50:46 +0000

    Stage background changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int gold;
     public TextMeshProUGUI goldText;
 
+    public Sprite[] stages;
+    private int currentStage;
+    private int enemiesUntilStageChange;
+    public Image stageImage;
+
     public static GameManager instance;
 
     void Awake()
     {
         instance = this;
+        enemiesUntilStageChange = 5;
     }
 
     public void AddGold(int amount)
     {
         gold += amount;
         goldText.text = "Gold: " + gold.ToString();
+    }
+
+    public void StageCheck()
+    {
+        enemiesUntilStageChange--;
+
+        if(enemiesUntilStageChange == 0){
+            enemiesUntilStageChange = 5;
+            currentStage++;
+
+            if(currentStage == stages.Length){
+                currentStage = 0;
+            }
+
+            stageImage.sprite = stages[currentStage];
+        }
     }
 }
