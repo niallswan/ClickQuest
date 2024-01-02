1672e24e1fcd9c0fd68142b0670cbefcf350a938
Niall Swan <niallswan@outlook.com>
2024-01-02 22:11:33 +0000

    Base enemy script and 3 enemy prefabs added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public Image healthBarFill;
+    public int currentHP;
+    public int maxHP;
+    public int goldToGive;
+
+    public void Damage()
+    {
+        currentHP--;
+        healthBarFill.fillAmount = (float)currentHP / (float)maxHP;
+
+        if(currentHP <= 0){
+            Defeated();
+        }
+    }
+
+    public void Defeated()
+    {
+        Debug.Log("Defeated");
+    }
+}
